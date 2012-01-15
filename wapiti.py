7046eabfc02bd757e0fea8f148a3efb90d1a377a
Adam Svanberg <asvanberg@gmail.com>
2012-01-15 10:25:07 +0100

    Cleanup and very basic testing

--- wapiti.py
+++ wapiti.py
@@ -109,7 +109,7 @@ ALGORITHMS = [
     'rprop-',
     'auto',
 ]
-    
+
 
 
 class ModelType(ctypes.Structure):
@@ -117,33 +117,33 @@ class ModelType(ctypes.Structure):
         ('opt', ctypes.POINTER(OptType)),    # options for training
 
         # Size of various model parameters
-        ('nlbl', ctypes.c_size_t),   # Y   number of labels       
-        ('nobs', ctypes.c_size_t),   # O   number of observations 
-        ('nftr', ctypes.c_size_t),   # F   number of features     
+        ('nlbl', ctypes.c_size_t),   # Y   number of labels
+        ('nobs', ctypes.c_size_t),   # O   number of observations
+        ('nftr', ctypes.c_size_t),   # F   number of features
 
         # Information about observations
-        ('kind', ctypes.c_char_p),   # [O]  observations type     
+        ('kind', ctypes.c_char_p),   # [O]  observations type
         ('uoff', ctypes.c_void_p),   # [O]  unigram weights offset
-        ('boff', ctypes.c_void_p),   # [O]  bigram weights offset 
+        ('boff', ctypes.c_void_p),   # [O]  bigram weights offset
 
         # The model itself
         ('theta', ctypes.c_void_p),  # [F]  features weights
 
         # Datasets
-        ('train', ctypes.c_void_p),  # training dataset    
+        ('train', ctypes.c_void_p),  # training dataset
         ('devel', ctypes.c_void_p),  # development dataset
-        ('reader', ctypes.c_void_p), 
+        ('reader', ctypes.c_void_p),
 
         # Stopping criterion
         ('werr', ctypes.c_void_p),   # Window of error rate of last iters
-        ('wcnt', ctypes.c_int),      # Number of iters in the window     
-        ('wpos', ctypes.c_int),      # Position for the next iter        
+        ('wcnt', ctypes.c_int),      # Number of iters in the window
+        ('wpos', ctypes.c_int),      # Position for the next iter
 
         # Timing
         ('timer', TmsType),
         ('total', ctypes.c_double)]
-    
-                
+
+
 def freeing_char_p(char_p):
     """
     Restype to copy a c_char_p to str and free memory
@@ -155,7 +155,7 @@ def freeing_char_p(char_p):
     s = ctypes.string_at(char_p)
     _libc.free(char_p)
     return s
-        
+
 # Setup methods
 
 # Model
@@ -182,7 +182,7 @@ _libc.free.argtypes = [ctypes.c_void_p]
 class NullHandler(logging.Handler):
     def emit(self, record):
         pass
-    
+
 logger = logging.getLogger('wapiti')
 logger.addHandler(NullHandler()) # Avoids warnings in non-logging applications
 
@@ -198,26 +198,34 @@ api_logs[2] = LogFunc(logger.warning)                # api_logs[WARNING]
 api_logs[3] = LogFunc(logger.info)                   # api_logs[INFO]
 
 
-def bio_safe(unicode_string):
-    """Remap whitespace to suitable unicode representations"""
-    return unicode_string.translate({
-        32:u'\u2192', # ' '
-        10:u'\u21b5', # '\n'
-         9:u'\u21a6', # '\t'
-        11:u'\u219d', # '\x0b'
-        12:u'\u219d', # '\x0c'
-        13:u'\u219d', # '\r'
-    })
-
-
 class Model:
-    
+    r"""
+    Wapiti model wrapper that handles training and labeling
+
+    The model takes BIO-formatted strings as input for both training
+    and labeling. Since wapiti treats all whitespace as column
+    delimiters, any whitespace in the token being labeled or the label
+    must be escaped, or weird and not so wonderful things will happen.
+
+    The patterns used to extract features are passed in as a string
+    with one pattern per line. See http://crfpp.sourceforge.net/#templ
+    for an explanation of the pattern format. There are also good
+    examples in the data directory of wapiti.
+
+    Example:
+    >>> m = Model(patterns='*\nU:Wrd X=%x[0,0]')
+    >>> m.add_training_sequence('We PRPsaw VBD\nthe DT\nlittle JJ\nyellow JJ\ndog NN')
+    >>> m.train()
+    >>> m.label_sequence('We\saw\nthe\nlittle\nyellow\ndog')
+    'We\\saw\tVBD\nthe\tDT\nlittle\tJJ\nyellow\tJJ\ndog\tNN\n'
+    """
+
     def __init__(self, patterns=None, encoding='utf8', **options):
 
         # Make sure encoding is taken care of when passing strings
         self.encoding = encoding
         ctypes.set_conversion_mode(encoding, 'replace')
-        
+
         if 'nthread' not in options:
             # If thread count isn't given, use number of processors
             options['nthread'] = multiprocessing.cpu_count()
@@ -248,7 +256,7 @@ class Model:
             self._model = _wapiti.api_new_model(
                 ctypes.pointer(self.options),
                 self.patterns
-            )            
+            )
 
 
     def __del__(self):
@@ -256,20 +264,23 @@ class Model:
             _wapiti.api_free_model(self._model)
 
     def add_training_sequence(self, sequence):
+        """
+        Add a string of BIO-formatted lines to the training set
+        """
         _wapiti.api_add_train_seq(self._model, sequence)
-        
+
     def train(self, sequences=[]):
         for seq in sequences:
             _wapiti.api_add_train_seq(self._model, seq)
         _wapiti.api_train(self._model)
-        
+
 
     def save(self, modelfile):
         if type(modelfile) != file:
             modelfile = open(modelfile, 'w')
         fp = ctypes.pythonapi.PyFile_AsFile(modelfile)
         _wapiti.api_save_model(self._model, fp)
-    
+
     def label_sequence(self, lines):
         """
         Accepts a string of BIO-formatted lines and adds a label column
@@ -278,7 +289,7 @@ class Model:
         linebreaks are ignored.
         """
         labeled = _wapiti.api_label_seq(self._model, lines)
-        return labeled 
+        return labeled
 
 
 if __name__ == '__main__':
@@ -291,74 +302,74 @@ if __name__ == '__main__':
     import optparse
 
     parser = optparse.OptionParser(
-        "usage: %prog train|label [options] [infile] [outfile]"
+        "usage: %prog train|label|test [options] [infile] [outfile]"
     )
 
     option_dict = {}
     def dictsetter(option, opt_str, value, *args, **kwargs):
         option_dict[option.dest] = value
 
-    parser.add_option('--me', dest='maxent', type='int',  
+    parser.add_option('--me', dest='maxent', type='int',
                       action='callback', callback=dictsetter)
     parser.add_option('--type', dest='type', type='string',
                       action='callback', callback=dictsetter)
-    parser.add_option('--algo', dest='algo', type='string',  
+    parser.add_option('--algo', dest='algo', type='string',
                       action='callback', callback=dictsetter)
-    parser.add_option('--pattern', dest='pattern', type='string',  
+    parser.add_option('--pattern', dest='pattern', type='string',
                       action='callback', callback=dictsetter)
-    parser.add_option('--model', dest='model', type='string',  
+    parser.add_option('--model', dest='model', type='string',
                       action='callback', callback=dictsetter)
-    parser.add_option('--devel', dest='devel', type='string',  
+    parser.add_option('--devel', dest='devel', type='string',
                       action='callback', callback=dictsetter)
-    parser.add_option('--compact', dest='compact', type='int',  
+    parser.add_option('--compact', dest='compact', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--sparse', dest='sparse', type='int',  
+    parser.add_option('--sparse', dest='sparse', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--nthread', dest='nthread', type='int',  
+    parser.add_option('--nthread', dest='nthread', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--jobsize', dest='jobsize', type='int',  
+    parser.add_option('--jobsize', dest='jobsize', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--maxiter', dest='maxiter', type='int',  
+    parser.add_option('--maxiter', dest='maxiter', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--rho1', dest='rho1', type='float',  
+    parser.add_option('--rho1', dest='rho1', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--rho2', dest='rho2', type='float',  
+    parser.add_option('--rho2', dest='rho2', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--objwin', dest='objwin', type='int',  
+    parser.add_option('--objwin', dest='objwin', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--stopwin', dest='stopwin', type='int',  
+    parser.add_option('--stopwin', dest='stopwin', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--stopeps', dest='stopeps', type='float',  
+    parser.add_option('--stopeps', dest='stopeps', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--clip', dest='clip', type='int',  
+    parser.add_option('--clip', dest='clip', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--histsz', dest='clip', type='int',  
+    parser.add_option('--histsz', dest='clip', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--maxls', dest='clip', type='int',  
+    parser.add_option('--maxls', dest='clip', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--eta0', dest='eta0', type='float',  
+    parser.add_option('--eta0', dest='eta0', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--alpha', dest='alpha', type='float',  
+    parser.add_option('--alpha', dest='alpha', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--kappa', dest='kappa', type='float',  
+    parser.add_option('--kappa', dest='kappa', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--stpmin', dest='stpmin', type='float',  
+    parser.add_option('--stpmin', dest='stpmin', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--stpmax', dest='stpmax', type='float',  
+    parser.add_option('--stpmax', dest='stpmax', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--stpinc', dest='stpinc', type='float',  
+    parser.add_option('--stpinc', dest='stpinc', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--stpdec', dest='stpdec', type='float',  
+    parser.add_option('--stpdec', dest='stpdec', type='float',
                       action='callback', callback=dictsetter)
-    parser.add_option('--label', dest='label', type='int',  
+    parser.add_option('--label', dest='label', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--check', dest='check', type='int',  
+    parser.add_option('--check', dest='check', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--score', dest='outsc', type='int',  
+    parser.add_option('--score', dest='outsc', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--post', dest='lblpost', type='int',  
+    parser.add_option('--post', dest='lblpost', type='int',
                       action='callback', callback=dictsetter)
-    parser.add_option('--nbest', dest='nbest', type='int',  
+    parser.add_option('--nbest', dest='nbest', type='int',
                       action='callback', callback=dictsetter)
 
     options, args = parser.parse_args()
@@ -375,7 +386,7 @@ if __name__ == '__main__':
     if action == 'train':
         model.train(infile.read().strip().split('\n\n'))
         model.save(outfile)
-            
+
     elif action == 'label':
         if 'model' not in option_dict:
             parser.error("Labeling requires a model")
@@ -384,9 +395,12 @@ if __name__ == '__main__':
         for sequence in infile.read().split('\n\n'):
             outfile.write(model.label_sequence(sequence)+'\n')
         outfile.close()
+    elif action == 'test':
+        import doctest
+        doctest.testmod()
     else:
         parser.error("Invalid action")
 
-        
-        
-    
+
+
+
